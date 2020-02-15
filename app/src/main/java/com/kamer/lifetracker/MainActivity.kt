cacdd57ed408f89a80a2bf977e570cd547cb1865
Maksim Dvoretskii <kamerok@gmail.com>
2020-02-15 17:42:28 +0300

    Implement google sign in

--- app/src/main/java/com/kamer/lifetracker/MainActivity.kt
+++ app/src/main/java/com/kamer/lifetracker/MainActivity.kt
@@ -1,62 +1,72 @@
 package com.kamer.lifetracker
 
-import android.Manifest
-import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        testView.setOnClickListener {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.GET_ACCOUNTS)
-                    != PackageManager.PERMISSION_GRANTED) {
-                requestPermission()
-            } else {
-                requestData()
-            }
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        signInView.setOnClickListener {
+            startActivityForResult(googleSignInClient.signInIntent, SIGN_IN)
         }
     }
 
-    override fun onRequestPermissionsResult(
-            requestCode: Int,
-            permissions: Array<out String>,
-            grantResults: IntArray
-    ) {
+    override fun onStart() {
+        super.onStart()
+        updateUi()
+    }
+
+    private fun updateUi() {
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        statusView.text = if (account != null) "signed" else "unsigned"
+        signInView.isVisible = account == null
+        testView.isVisible = account != null
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         when (requestCode) {
-            MY_PERMISSIONS_REQUEST_READ_CONTACTS -> {
-                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    requestData()
-                } else {
-                    Toast.makeText(this, "No permission", Toast.LENGTH_SHORT).show()
-                }
-                return
+            SIGN_IN -> {
+                // The Task returned from this call is always completed, no need to attach
+                // a listener.
+                val task: Task<GoogleSignInAccount> =
+                    GoogleSignIn.getSignedInAccountFromIntent(data)
+                handleSignInResult(task)
             }
         }
     }
 
-    private fun requestPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.GET_ACCOUNTS)) {
-            Toast.makeText(this, "Give permission please", Toast.LENGTH_SHORT).show()
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            // Signed in successfully, show authenticated UI.
+        } catch (e: ApiException) { // The ApiException status code indicates the detailed failure reason.
+// Please refer to the GoogleSignInStatusCodes class reference for more information.
         }
-        ActivityCompat.requestPermissions(this,
-                arrayOf(Manifest.permission.GET_ACCOUNTS),
-                MY_PERMISSIONS_REQUEST_READ_CONTACTS
-        )
-    }
-
-    private fun requestData() {
-
+        updateUi()
     }
 
     companion object {
         private const val MY_PERMISSIONS_REQUEST_READ_CONTACTS = 100
+        private const val SIGN_IN = 10
     }
 }
