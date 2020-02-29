e3f36d51c1a35664fcd68a295c5d6bc314834087
Maksim Dvoretskii <kamerok@gmail.com>
2020-03-01 00:44:46 +0300

    Load data after login

--- app/src/main/java/com/kamer/lifetracker/MainActivity.kt
+++ app/src/main/java/com/kamer/lifetracker/MainActivity.kt
@@ -5,16 +5,17 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.fragment.app.commit
+import androidx.lifecycle.coroutineScope
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.common.api.Scope
 import com.google.android.gms.tasks.Task
 import com.google.api.services.sheets.v4.SheetsScopes
 import com.kamer.lifetracker.records.RecordsFragment
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.launch
 import java.lang.ref.WeakReference
 
 
@@ -68,13 +69,15 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     }
 
     private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
-        try {
-            val account = completedTask.getResult(ApiException::class.java)
-            // Signed in successfully, show authenticated UI.
-        } catch (e: ApiException) { // The ApiException status code indicates the detailed failure reason.
+        lifecycle.coroutineScope.launch {
+            try {
+                DataProvider.updateData()
+                // Signed in successfully, show authenticated UI.
+            } catch (e: Exception) { // The ApiException status code indicates the detailed failure reason.
 // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            }
+            updateUi()
         }
-        updateUi()
     }
 
     companion object {
